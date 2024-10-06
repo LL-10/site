2c8e5e42fb6e1aafa49b558b467141a7840c7ba9
Lorenzo Lomartire <lorenzolomartire13@gmail.com>
2024-10-06 12:16:14 +0200

    modificato:             docs/index.html 	modificato:             docs/script.js

--- docs/script.js
+++ docs/script.js
@@ -1,33 +1,29 @@
-let installPrompt = null;
-const installButton = document.querySelector("#install");
+const installButton = document.getElementById('install');
+const openButton = document.getElementById('open');
 
-window.addEventListener("beforeinstallprompt", (event) => {
+window.addEventListener('beforeinstallprompt', async (event) => {
+	const relatedApps = await navigator.getInstalledRelatedApps();
+	document.body.innerHTML = relatedApps;
 	event.preventDefault();
-	installPrompt = event;
-	installButton.removeAttribute("hidden");
+	installButton.prompt = event;
+	installButton.removeAttribute('hidden');
 });
 
-installButton.addEventListener("click", async () => {
-	if (!installPrompt) {
+installButton.addEventListener('click', async () => {
+	if (!installButton.prompt)
 		return;
-	}
 	const result = await installPrompt.prompt();
-	console.log(`Install prompt was: ${result.outcome}`);
+	console.log('Install prompt was: ' + result.outcome);
+	disableInAppInstallPrompt();
+});
+
+window.addEventListener('appinstalled', () => {
 	disableInAppInstallPrompt();
 });
 
 function disableInAppInstallPrompt() {
-	installPrompt = null;
-	installButton.setAttribute("hidden", "");
-}
-
-window.addEventListener("appinstalled", () => {
-	disableInAppInstallPrompt();
-});
-
-function disableInAppInstallPrompt() {
-	installPrompt = null;
-	installButton.setAttribute("hidden", "");
+	installButton.prompt = null;
+	installButton.setAttribute('hidden', '');
 }
 
 /*const relatedApps = await navigator.getInstalledRelatedApps();
@@ -37,7 +33,7 @@ const psApp = relatedApps.find((app) => app.id === "com.example.myapp");
 
 if (psApp) {
 	// Update UI as appropriate
-}*/
+}
 
 window.addEventListener("beforeinstallprompt", async (event) => {
 	const relatedApps = await navigator.getInstalledRelatedApps();
@@ -50,3 +46,4 @@ window.addEventListener("beforeinstallprompt", async (event) => {
 		// Update UI as appropriate
 	}
 });
+*/
