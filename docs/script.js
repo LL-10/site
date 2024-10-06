862c3b9fbffe74e53a23c40e0802bb8d6100d604
Lorenzo Lomartire <lorenzolomartire13@gmail.com>
2024-10-06 12:42:40 +0200

    modificato:             docs/script.js

--- docs/script.js
+++ docs/script.js
@@ -2,12 +2,6 @@ const installButton = document.getElementById('install');
 const openButton = document.getElementById('open');
 
 window.addEventListener('beforeinstallprompt', async (event) => {
-	const relatedApps = await navigator.getInstalledRelatedApps();
-	document.body.innerHTML = relatedApps;
-	if(relatedApps.find((app) => app.id === 'll-10.github.io/site')) {
-		openButton.removeAttribute('hidden');
-		return;
-	}
 	event.preventDefault();
 	installButton.prompt = event;
 	installButton.removeAttribute('hidden');
@@ -18,36 +12,11 @@ installButton.addEventListener('click', async () => {
 		return;
 	const result = await installPrompt.prompt();
 	console.log('Install prompt was: ' + result.outcome);
-	disableInAppInstallPrompt();
+	installButton.prompt = null;
+	installButton.setAttribute('hidden', '');
 });
 
 window.addEventListener('appinstalled', () => {
-	disableInAppInstallPrompt();
-});
-
-function disableInAppInstallPrompt() {
 	installButton.prompt = null;
 	installButton.setAttribute('hidden', '');
-}
-
-/*const relatedApps = await navigator.getInstalledRelatedApps();
-
-// Search for a specific installed platform-specific app
-const psApp = relatedApps.find((app) => app.id === "com.example.myapp");
-
-if (psApp) {
-	// Update UI as appropriate
-}
-
-window.addEventListener("beforeinstallprompt", async (event) => {
-	const relatedApps = await navigator.getInstalledRelatedApps();
-
-	// Search for a specific installed platform-specific app
-	const psApp = relatedApps.find((app) => app.id === "com.example.myapp");
-
-	if (psApp) {
-		event.preventDefault();
-		// Update UI as appropriate
-	}
 });
-*/
